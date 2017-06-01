328a58bc83abd2cc174bde5419bdc84c809f48c3
E Shifrin <eshifrin@gmail.com>
2017-05-31 23:15:16 -0700

    deleted sampleMap

--- src/routes/BikeMap/components/BikeMap.js
+++ src/routes/BikeMap/components/BikeMap.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import InputLocation from './InputLocation'
 import SimpleMapExample from './testMap'
-import ExampleMap from './ExampleMap'
 import CustomSearchBox from './customSearchBox'
 
 //do we need start and end feeding the input boxes? its a 1 way data flow
