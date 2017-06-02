6be59cbac7cbe6d973b951935251b697e56d3065
E Shifrin <eshifrin@gmail.com>
2017-06-02 06:25:38 -0700

    fixed server route

--- server/routes/directions.js
+++ server/routes/directions.js
@@ -3,7 +3,9 @@ const getDirections = require('../google').getDirections
 //add logger
 module.exports = (req, res, next) => {
   getDirections(req.query.start, req.query.end)
-  .then(res.json)
+  .then(results => {
+    res.json(results)
+  })
   .catch(err => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
