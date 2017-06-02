7f04f561f8bfde25e6694c6ad70bd83797322955
Steven Reed <steve.t.reed@gmail.com>
2017-06-02 10:01:25 -0700

    Update google.js

--- server/google.js
+++ server/google.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const Promise = require('bluebird').Promise;
 const googleMaps = require('@google/maps').createClient({
-  key: 'AIzaSyBTd3YnPAF_bno0NQfKjmNsTL6BKlaqK3I',
+  key: process.env.googleAPI,
   Promise: Promise
 });
 
