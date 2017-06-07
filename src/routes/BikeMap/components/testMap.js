4ad97010498992e477a0f949a5016a46a0084323
Streed12 <steve.t.reed@gmail.com>
2017-06-07 07:57:38 -0700

    Removed sample route.

--- src/routes/BikeMap/components/testMap.js
+++ src/routes/BikeMap/components/testMap.js
@@ -6,59 +6,6 @@ import {
   unmountComponentAtNode,
 } from "react-dom";
 
-// let samplePath = 
-//   [{
-//         "lat": 37.7690564,
-//         "lng": -122.4506363
-//       },
-//       {
-//         "lat": 37.7695354,
-//         "lng": -122.4507359
-//       },
-//       {
-//         "lat": 37.7696002,
-//         "lng": -122.450244
-//       },
-//       {
-//         "lat": 37.770529,
-//         "lng": -122.4504334
-//       },
-//       {
-//         "lat": 37.7739163,
-//         "lng": -122.4238142
-//       },
-//       {
-//         "lat": 37.7767142,
-//         "lng": -122.4243971
-//       },
-//       {
-//         "lat": 37.7766779,
-//         "lng": -122.4246641
-//       },
-//       {
-//         "lat": 37.777618,
-//         "lng": -122.4248359
-//       },
-//       {
-//         "lat": 37.7784944,
-//         "lng": -122.4182568
-//       },
-//       {
-//         "lat": 37.7831079,
-//         "lng": -122.419195
-//       },
-//       {
-//         "lat": 37.7844603,
-//         "lng": -122.4085608
-//       },
-//       {
-//         "lat": 37.7840081,
-//         "lng": -122.408092
-//       },
-//       {
-//         "lat": 37.7834842,
-//         "lng": -122.4087433
-//       }];
 
 // Wrap all `react-google-maps` components with `withGoogleMap` HOC
 // and name it GettingStartedGoogleMap
@@ -188,7 +135,7 @@ class SimpleMapExample extends Component {
       })
     })
   }
-  handleClose(){
+  // handleClose(){
     // let { iWindow } = this.state;
     // this.setState({
     //   iWindow: {
@@ -198,7 +145,7 @@ class SimpleMapExample extends Component {
     // }, () => {
     //   console.log(this.state)
     // })
-  }
+  // }
   handleMapMounted(map) {
     this._map = map;
   }
